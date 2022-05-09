0430b964f47e3747c55402716db868d250d3391b
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-05-09 16:50:33 -0700

    add it all

---
+++ server/game/polygonscan/types.d.ts
@@ -0,0 +1,29 @@
+export interface IPolygonResult {
+  nonce?: string
+  blockHash?: string
+  transactionIndex?: string
+  gasPrice?: string
+  txreceipt_status?: string
+  cumulativeGasUsed?: string
+  confirmations?: string
+  blockNumber: string
+  timestamp: string
+  hash: string
+  from: string
+  to: string
+  value: string
+  contractAddress: string
+  input: string
+  type: string
+  gas: string
+  gasUsed: string
+  traceId: string
+  isError: string
+  errCode: string
+}
+
+export interface IPolygonResponse {
+  status: string
+  message: string
+  result: IPolygonResult[]
+}
